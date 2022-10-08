f071d4a161dd5455fee3b39b3e0a016092b0f0ad
mihail tilica <mihailtilica1@gmail.com>
2022-10-08 12:38:06 +0300

    a

--- controllers/stats.js
+++ controllers/stats.js
@@ -3,7 +3,7 @@ import User from "../models/User.js"
 export const getCreatedProjectsCount = async (req, res, next) => {
     try {
         const user = await User.findById(req.user.id) 
-        res.status(200).json({ count: user?.createdProjectsIds.length})
+        res.status(200).json({ count: user?.createdProjectsIds.length })
     } catch (err) {
         next(err)
     }
@@ -12,7 +12,7 @@ export const getCreatedProjectsCount = async (req, res, next) => {
 export const getSignedUpProjectsCount = async (req, res, next) => {
     try {
         const user = await User.findById(req.user.id) 
-        res.status(200).json({ count: user?.signedUpProjectsIds.length})
+        res.status(200).json({ count: user?.signedUpProjectsIds.length })
     } catch (err) { 
         next(err)
     }
